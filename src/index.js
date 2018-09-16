412b8376d5d14329cf662aa46bca3934d6d987a8
Владимир Зубович <moraviin@gmail.com>
2018-09-16 21:11:58 +0300

    9th class

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  let arr = equation.match(/-? ?\d+/g);
+  let nums=arr.map(function(num){
+    return parseInt(num.replace(' ',''));    
+  })
+  let D=Math.sqrt(nums[2]*nums[2]-4*nums[0]*nums[3])/(2*nums[0]);
+  let x=(-nums[2])/(2*nums[0]);
+  let answer=[Math.round(x-D),Math.round(x+D)];// your implementation
+  let compare=(a,b)=>(a-b);
+  return answer.sort(compare);
 }
